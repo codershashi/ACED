0c47a9a45ba723c07f1f474210608e2eb69f7b5f
shashi <52621873+codershashi@users.noreply.github.com>
2023-10-30 20:43:50 +0530

    created bugModel and added bug data to it

--- bugProject/bug/models.py
+++ bugProject/bug/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class bugModel(models.Model):
+    bugTypeChoices = [
+        ("err", "Error"),
+        ("feature", "New Feature"),
+        ("enhancement", "Enhancement"),
+        ("security", "Security Issue"),
+        ("performance", "Performance Issue"),
+        ("design", "Design Issue"),
+        ("documentation", "Documentation Issue"),
+    ]
+    statusChoices = [
+        ("to_do", "To Do"),
+        ("in_progress", "In Progress"),
+        ("done", "Done"),
+        ("on_hold", "On Hold"),
+        ("invalid", "Invalid"),
+        ("duplicate", "Duplicate"),
+        ("wontfix", "Won't Fix"),
+    ]
+    description: models.TextField()
+    bug_type: models.CharField(max_length=15, choices=bugTypeChoices)
+    report_date: models.DateField()
+    status: models.CharField(max_length=15, choices=statusChoices)
+
--- bugProject/bugProject/settings.py
+++ bugProject/bugProject/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "bug.apps.BugConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bug",
+    
 ]
 
 MIDDLEWARE = [
